2b44e980d275b73126293ba56b565feec72b4346
Tushar Talwekar <tushartalwekar08@gmail.com>
2025-01-09 08:29:24 +0530

    Adding new files

--- src/section_05_introduction_to_java/Example1.java
+++ src/section_05_introduction_to_java/Example1.java
@@ -3,5 +3,6 @@ import java.lang.*;
 public class Example1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 }
